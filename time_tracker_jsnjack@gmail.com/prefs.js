693d70a64ff9e7ea6322365474b1f39c6bae79a9
Yauhen Shulitski <jsnjack@gmail.com>
2015-09-28 23:28:43 +0200

    Adjust pause time

--- time_tracker_jsnjack@gmail.com/prefs.js
+++ time_tracker_jsnjack@gmail.com/prefs.js
@@ -32,6 +32,23 @@ function adjust_start_time(hours, mins) {
     settings.set_boolean("update-start-time", true);
 }
 
+function adjust_pause(hours, mins) {
+    // Update pause-duration
+    var change = hours * 60 * 60 * 1000 + mins * 60 * 1000;
+    settings.set_int('pause-duration', change);
+    // Mark time for update
+    settings.set_boolean("update-start-time", true);
+}
+
+function parse_pause_duration(seconds) {
+    // Returns amount of hours and minutes
+    var data = {};
+    data.hours = parseInt(seconds / 3600, 10);
+    seconds = seconds - data.hours * 3600;
+    data.mins = parseInt(seconds / 60, 10);
+    return data;
+}
+
 function save_color_change(color, key) {
     // Saves changes to the settings
     settings.set_string(key, color.to_string());
@@ -44,13 +61,14 @@ function buildPrefsWidget() {
             orientation: Gtk.Orientation.VERTICAL,
             border_width: 10
             }),
+        data = parse_pause_duration(settings.get_int('pause-duration') / 1000),
         display_label = new Gtk.Label({label: "<b>" + _("Display") + "</b>", use_markup: true, xalign: 0}),
         display_box = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL, margin_left: 20}),
         seconds_label = new Gtk.Label({label: _("Show seconds"), margin_right: 10}),
         seconds_switch = new Gtk.Switch({active: settings.get_boolean('show-seconds')}),
 
         adjust_time_label = new Gtk.Label({label: "<b>" + _("Adjust start time") + "</b>", use_markup: true, xalign: 0, margin_top: 20}),
-        start_time_label = new Gtk.Label({label: _("Current start time: ") + start_time.toLocaleString()}),
+        start_time_label = new Gtk.Label({label: _("Start time: ") + start_time.toLocaleString()}),
         adjust_time_box = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL, margin_left: 20}),
         hours_box = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL}),
         hours_label = new Gtk.Label({label: _("Hours"), margin_right: 10}),
@@ -66,6 +84,24 @@ function buildPrefsWidget() {
                                                                        upper: 60,
                                                                        step_increment: 1}),
                                        value: 0}),
+
+        adjust_pause_label = new Gtk.Label({label: "<b>" + _("Adjust pause duration") + "</b>", use_markup: true, xalign: 0, margin_top: 20}),
+        adjust_pause_box = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL, margin_left: 20}),
+        pause_hours_box = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL}),
+        pause_hours_label = new Gtk.Label({label: _("Hours"), margin_right: 10}),
+        pause_hours_spin = new Gtk.SpinButton({adjustment: new Gtk.Adjustment({value: data.hours,
+                                                                        lower: -999,
+                                                                        upper: 999,
+                                                                        step_increment: 1}),
+                                        value: 0}),
+        pause_mins_box = new Gtk.Box({orientation: Gtk.Orientation.HORIZONTAL}),
+        pause_mins_label = new Gtk.Label({label: _("Minutes"), margin_right: 10}),
+        pause_mins_spin = new Gtk.SpinButton({adjustment: new Gtk.Adjustment({value: data.mins,
+                                                                       lower: -60,
+                                                                       upper: 60,
+                                                                       step_increment: 1}),
+                                       value: 0}),
+
         color_label = new Gtk.Label({label: "<b>" + _("Colours") + "</b>", use_markup: true, xalign: 0}),
         color_box = new Gtk.Box({orientation: Gtk.Orientation.VERTICAL, margin_left: 20}),
         indicator_color = new Gtk.ColorButton({
@@ -91,6 +127,13 @@ function buildPrefsWidget() {
     adjust_time_box.pack_start(hours_box, false, false, 3);
     adjust_time_box.pack_start(mins_box, false, false, 3);
 
+    pause_hours_box.pack_start(pause_hours_label, false, false, 3);
+    pause_hours_box.pack_start(pause_hours_spin, false, false, 3);
+    pause_mins_box.pack_start(pause_mins_label, false, false, 3);
+    pause_mins_box.pack_start(pause_mins_spin, false, false, 3);
+    adjust_pause_box.pack_start(pause_hours_box, false, false, 3);
+    adjust_pause_box.pack_start(pause_mins_box, false, false, 3);
+
     indicator_color_box.pack_start(indicator_color_label, false, false, 3);
     indicator_color_box.pack_start(indicator_color, false, false, 3);
     indicator_paused_color_box.pack_start(indicator_paused_color_label, false, false, 3);
@@ -104,8 +147,13 @@ function buildPrefsWidget() {
     widget.pack_start(adjust_time_label, false, false, 3);
     widget.pack_start(start_time_label, false, false, 3);
     widget.pack_start(adjust_time_box, false, false, 3);
+    widget.pack_start(adjust_pause_label, false, false, 3);
+    widget.pack_start(adjust_pause_box, false, false, 3);
     widget.pack_start(color_label, false, false, 3);
     widget.pack_start(color_box, false, false, 3);
+
+    pause_hours_spin.set_sensitive(!settings.get_boolean("paused"));
+    pause_mins_spin.set_sensitive(!settings.get_boolean("paused"));
 
     //Callbacks
     seconds_switch.connect("notify::active", function (state) {
@@ -116,6 +164,12 @@ function buildPrefsWidget() {
     });
     mins_spin.connect("value-changed", function () {
         adjust_start_time(hours_spin.get_value(), mins_spin.get_value());
+    });
+    pause_hours_spin.connect("value-changed", function () {
+        adjust_pause(pause_hours_spin.get_value(), pause_mins_spin.get_value());
+    });
+    pause_mins_spin.connect("value-changed", function () {
+        adjust_pause(pause_hours_spin.get_value(), pause_mins_spin.get_value());
     });
     indicator_color.connect("color-set", function () {
         save_color_change(indicator_color.get_rgba(), "indicator-color");
