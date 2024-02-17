ee8b6775aef1e58eb071be66fca11982e9d6a51a
Vlad <0vvland@gmail.com>
2024-02-17 21:28:31 +0300

    Refactor to use popup menu instead notifications

--- time_tracker_jsnjack@gmail.com/extension.js
+++ time_tracker_jsnjack@gmail.com/extension.js
@@ -2,206 +2,179 @@
 /* exported init, enable, disable */
 import * as Main from 'resource:///org/gnome/shell/ui/main.js';
 import GLib from 'gi://GLib';
-import * as MessageTray from 'resource:///org/gnome/shell/ui/messageTray.js';
+import Clutter from 'gi://Clutter';
 import St from 'gi://St';
 import GObject from 'gi://GObject';
-import { Extension, gettext as _ } from 'resource:///org/gnome/shell/extensions/extension.js';
+import {
+  Extension,
+  gettext as _,
+} from 'resource:///org/gnome/shell/extensions/extension.js';
+import * as PanelMenu from 'resource:///org/gnome/shell/ui/panelMenu.js';
 
-let start_time, indicator, start_time_string, settings, timeout,
-    self,
-    preferences_button_name,
-    restart_button_name,
-    toggle_button_name;
+let start_time, start_time_string;
 
-const TTNotificationBanner = GObject.registerClass({
-    GTypeName: 'MyNSource'
-}, class TTNotificationBanner extends MessageTray.NotificationBanner {
-    addAction(label, callback) {
-        // Style buttons
-        let extra_style = "";
+const TimeTracker = GObject.registerClass(
+  class TimeTracker extends PanelMenu.Button {
+    _init (extention) {
+      super._init(0.5, 'Time tracker', false);
+      this._extention = extention;
+      this._settings = this._extention.getSettings();
+      this.set_style_class_name('panel-button');
 
-        // Redefining label is required for translation
-        label = _(label);
-        if (label === restart_button_name) {
-            extra_style = " button-restart";
-        } else if (label === toggle_button_name) {
-            if (settings.get_boolean("paused")) {
-                label = _("Resume");
-                extra_style = " button-resume";
-            } else {
-                label = _("Pause");
-                extra_style = " button-pause";
-            }
-        }
-        const button = new St.Button({
-            style_class: 'notification-button' + extra_style,
-            label: label,
-            x_expand: true,
-            can_focus: true
-        });
+      this._label = new St.Label({
+        text: 'Loading...',
+        can_focus: true,
+        x_align: Clutter.ActorAlign.CENTER,
+        x_expand: true,
+        y_align: Clutter.ActorAlign.CENTER,
 
-        return this.addButton(button, callback);
+      });
+      this.actor.add_actor(this._label);
+
+      // Get start_time from settings
+      start_time_string = this._settings.get_string('start-time');
+      start_time = new Date(start_time_string);
+      // If creation of Date object failed, create a new one
+      if (isNaN(start_time.getHours())) {
+        start_time = new Date();
+        this._settings.set_string('start-time', start_time.toString());
+      }
+      this._timeout = GLib.timeout_add(1000, GLib.PRIORITY_LOW, () => {
+        this.refresh();
+        return true;
+      });
+
+      const paused_by_screen_lock = this._settings.get_boolean(
+        'paused-by-screen-lock');
+      const paused = this._settings.get_boolean('paused');
+      if (paused_by_screen_lock && paused) {
+        this.onToggle();
+      }
+      this._settings.set_boolean('paused-by-screen-lock', false);
+
+      this._pauseMenu = this.menu.addAction(_('Resume'),
+        this.onToggle.bind(this));
+      this.menu.addAction(_('Restart'), this.onReset.bind(this));
+      this.menu.addAction(_('Preferences'),
+        (() => this._extention.openPreferences()).bind(this));
+
+      this.updateIndicatorStyle();
     }
-});
 
-const TTSource = GObject.registerClass({
-    GTypeName: 'MySource'
-}, class TTSource extends MessageTray.Source {
-    createBanner(notification) {
-        return new TTNotificationBanner(notification);
+    updateIndicatorStyle () {
+      if (!this._settings.get_boolean('paused')) {
+        this._label.set_style(
+          'color: ' + this._settings.get_string('indicator-color') + ';');
+        this._pauseMenu?.set_style(
+          'color: ' + this._settings.get_string('indicator-paused-color') +
+          ';');
+        this._pauseMenu?.label.set_text(_('Pause'));
+      } else {
+        this._label.set_style(
+          'color: ' + this._settings.get_string('indicator-paused-color') +
+          ';');
+        this._pauseMenu?.set_style(
+          'color: ' + this._settings.get_string('indicator-color') + ';');
+        this._pauseMenu?.label.set_text(_('Resume'));
+      }
     }
-});
 
-function _refresh() {
-    // Get difference between start time and current time
-    // and show it
-    let current_time = new Date(),
+    refresh () {
+      // Get difference between start time and current time
+      // and show it
+      let current_time = new Date(),
         difference, timer;
-    // Check if start_time needs update:
-    if (settings.get_boolean("update-start-time")) {
-        start_time_string = settings.get_string('start-time');
+      // Check if start_time needs update:
+      if (this._settings.get_boolean('update-start-time')) {
+        start_time_string = this._settings.get_string('start-time');
         start_time = new Date(start_time_string);
-        settings.set_boolean("update-start-time", false);
-    }
-    // Check if indicator style needs update:
-    if (settings.get_boolean("update-indicator-style")) {
-        update_indicator_style();
-        settings.set_boolean("update-indicator-style", false);
-    }
-    // If in pause, than show the difference between start_time and pause-start-time
-    if (settings.get_boolean("paused")) {
-        current_time = new Date(settings.get_string("pause-start-time"));
-    }
-    // Get difference between two times in secs
-    difference = Math.round((current_time - start_time - settings.get_int("pause-duration")) / 1000);
-    const hours = parseInt(difference / 3600, 10);
-    if (hours !== 0) {
+        this._settings.set_boolean('update-start-time', false);
+      }
+      // Check if indicator style needs update:
+      if (this._settings.get_boolean('update-indicator-style')) {
+        this.updateIndicatorStyle();
+        this._settings.set_boolean('update-indicator-style', false);
+      }
+      // If in pause, than show the difference between start_time and pause-start-time
+      if (this._settings.get_boolean('paused')) {
+        current_time = new Date(this._settings.get_string('pause-start-time'));
+      }
+      // Get difference between two times in secs
+      difference = Math.round(
+        (current_time - start_time - this._settings.get_int('pause-duration')) /
+        1000);
+      const hours = parseInt(difference / 3600, 10);
+      if (hours !== 0) {
         difference = difference - hours * 3600;
-    }
-    const mins = parseInt(difference / 60, 10);
-    if (mins !== 0) {
+      }
+      const mins = parseInt(difference / 60, 10);
+      if (mins !== 0) {
         difference = difference - mins * 60;
+      }
+      const secs = difference;
+      // Prepare timer info
+      if (this._settings.get_boolean('show-seconds') === true) {
+        timer = '%d:%02d:%02d'.format(hours, mins, secs);
+      } else {
+        timer = '%d:%02d'.format(hours, mins);
+      }
+      this._label.set_text(timer);
     }
-    const secs = difference;
-    // Prepare timer info
-    if (settings.get_boolean('show-seconds') === true) {
-        timer = "%d:%02d:%02d".format(hours, mins, secs);
-    } else {
-        timer = "%d:%02d".format(hours, mins);
-    }
-    indicator.set_label(timer);
-}
 
-function _restart() {
-    // Restart timer. Set new value for start_time
-    const message_body = start_time.toLocaleString(),
-        message_title = _("Timer was started at"),
-        source = new TTSource(_("Time tracker"), "preferences-system-time-symbolic"),
-        notification = new MessageTray.Notification(source, message_title, message_body);
-
-    // Add buttons
-    notification.addAction(preferences_button_name, on_preferences);
-    notification.addAction(toggle_button_name, on_toggle);
-    notification.addAction(restart_button_name, on_reset);
-
-    // Notification disapears from tray after it was showed
-    notification.setTransient(true);
-
-    Main.messageTray.add(source);
-    source.showNotification(notification);
-}
-
-function update_indicator_style() {
-    // Updates style of the indicator
-    if (!settings.get_boolean("paused")) {
-        indicator.set_style("color: " + settings.get_string("indicator-color") + ";");
-    } else {
-        indicator.set_style("color: " + settings.get_string("indicator-paused-color") + ";");
-    }
-}
-
-function on_preferences() {
-    // Show GNOME Shell preferences
-    self.openPreferences();
-    return 0;
-}
-
-function on_reset() {
-    start_time = new Date();
-    settings.set_string('start-time', start_time.toString());
-    settings.set_int('pause-duration', 0);
-    settings.set_boolean('paused', false);
-    update_indicator_style();
-}
-
-function on_toggle() {
-    // Handle pause button
-    let state = settings.get_boolean("paused"),
+    onToggle () {
+      // Handle pause button
+      let state = this._settings.get_boolean('paused'),
         current_time, pause_start_time;
-    if (!state) {
+      if (!state) {
         // Pause timer
         current_time = new Date();
-        settings.set_string('pause-start-time', current_time.toString());
-        settings.set_boolean("paused", true);
-    } else {
+        this._settings.set_string('pause-start-time', current_time.toString());
+        this._settings.set_boolean('paused', true);
+      } else {
         // Resume timer
         current_time = new Date();
-        pause_start_time = new Date(settings.get_string('pause-start-time'));
-        settings.set_int('pause-duration', settings.get_int('pause-duration') + (current_time - pause_start_time));
-        settings.set_boolean("paused", false);
-    }
-    update_indicator_style();
-}
-
-export default class TimeTracker extends Extension {
-    enable() {
-        preferences_button_name = _("Preferences");
-        restart_button_name = _("Restart");
-        toggle_button_name = _("Pause/Resume");
-
-        indicator = new St.Button({
-            style_class: 'panel-button',
-        });
-        settings = this.getSettings();
-        self = this;
-        update_indicator_style();
-        // Get start_time from settings
-        start_time_string = settings.get_string('start-time');
-        start_time = new Date(start_time_string);
-        // If creation of Date object failed, create a new one
-        if (isNaN(start_time.getHours())) {
-            start_time = new Date();
-            settings.set_string('start-time', start_time.toString());
-        }
-        indicator.set_label('Time Tracker');
-        timeout = GLib.timeout_add(1000, GLib.PRIORITY_LOW, function () {
-            _refresh();
-            return true;
-        });
-        indicator.connect('button-press-event', _restart);
-
-        Main.panel._rightBox.insert_child_at_index(indicator, 0);
-
-        const paused_by_screen_lock = settings.get_boolean('paused-by-screen-lock');
-        const paused = settings.get_boolean("paused");
-        if (paused_by_screen_lock && paused) {
-            on_toggle();
-        }
-        settings.set_boolean('paused-by-screen-lock', false);
+        pause_start_time = new Date(
+          this._settings.get_string('pause-start-time'));
+        this._settings.set_int('pause-duration',
+          this._settings.get_int('pause-duration') +
+          (current_time - pause_start_time));
+        this._settings.set_boolean('paused', false);
+      }
+      this.updateIndicatorStyle();
     }
 
-    disable() {
-        const pause_during_screen_lock = settings.get_boolean('pause-during-screen-lock');
-        const paused = settings.get_boolean("paused");
-        if (pause_during_screen_lock && !paused) {
-            on_toggle();
-            settings.set_boolean('paused-by-screen-lock', true);
-        }
-
-        indicator.destroy();
-        GLib.Source.remove(timeout);
-        indicator = null;
-        settings = null;
-        self = null;
+    onReset () {
+      start_time = new Date();
+      this._settings.set_string('start-time', start_time.toString());
+      this._settings.set_int('pause-duration', 0);
+      this._settings.set_boolean('paused', false);
+      this.updateIndicatorStyle();
     }
+
+    onDestroy () {
+      const pause_during_screen_lock = this._settings.get_boolean(
+        'pause-during-screen-lock');
+      const paused = this._settings.get_boolean('paused');
+      if (pause_during_screen_lock && !paused) {
+        this.onToggle();
+        this._settings.set_boolean('paused-by-screen-lock', true);
+      }
+      GLib.Source.remove(this._timeout);
+      this?.destroy();
+    }
+  });
+
+export default class TimeTrackerExtension extends Extension {
+  enable () {
+    this._indicator = new TimeTracker(this);
+
+    // Add the indicator to the panel
+    Main.panel.addToStatusArea(this.uuid, this._indicator, 0);
+    // Main.panel._rightBox.insert_child_at_index(this._indicator, 0);
+  }
+
+  disable () {
+    this._indicator.onDestroy();
+    this._indicator = null;
+  }
 }
